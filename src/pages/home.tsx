ae78ecd94de78b736db72e1fa3c2e53550abc363
Nitish Kumar Jena <jenanitish490@gmail.com>
2025-08-16 01:39:22 +0530

    16th 2nd

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,8 +2,15 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { Sparkles } from "lucide-react";
 import { motion } from "framer-motion";
+import { useEffect } from "react";
+import { getCookie } from "@/utils/getCookie";
 
 export default function LandingPage() {
+  useEffect(()=> {
+    const cookie = getCookie("token");
+    console.log("From kanding page cookie", cookie);
+    
+  }, []);
   return (
     <div className="min-h-screen bg-background p-4 sm:p-6">
       {/* <header className="flex flex-col sm:flex-row justify-between items-center py-6 gap-4">
