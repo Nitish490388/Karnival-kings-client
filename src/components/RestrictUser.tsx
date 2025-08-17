3fe81fe7c0cf902c73aa89f01df18ec40bb965f7
Nitish Kumar Jena <jenanitish490@gmail.com>
2025-08-17 10:40:26 +0530

    client test-2

--- src/components/RestrictUser.tsx
+++ src/components/RestrictUser.tsx
@@ -6,12 +6,12 @@ const RestrictUser = () => {
     const navigate = useNavigate();
     
     useEffect(() => {
-        const getCookie = (name: string): string | null => {
-          const value = `; ${document.cookie}`;
-          const parts = value.split(`; ${name}=`);
-          if (parts.length === 2) return parts.pop()?.split(';').shift() ?? null;
-          return null;
-        };
+        // const getCookie = (name: string): string | null => {
+        //   const value = `; ${document.cookie}`;
+        //   const parts = value.split(`; ${name}=`);
+        //   if (parts.length === 2) return parts.pop()?.split(';').shift() ?? null;
+        //   return null;
+        // };
       
         const token = "token is nitish"
         console.log(token);
